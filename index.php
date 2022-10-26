d986499cbd6fabb7202f87647a0bd236f2f9955b
milindvavare <36969321+milindvavare@users.noreply.github.com>
2022-10-26 12:57:42 +0530

    linkden-wrapping

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ show_source("https://www.linkedin.com/campaignmanager/accounts/508808364/creatives");
+//echo file_get_contents("https://www.linkedin.com/campaignmanager/accounts/508808364/creatives");;
+?>
+
+</body>
+</html>
